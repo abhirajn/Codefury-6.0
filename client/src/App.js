15d076e8f0fd07b5aa988403f2b87a6f59527e86
Premsagar Chakrapani <premsagarc11@gmail.com>
2023-09-23 14:22:24 +0530

    mes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Home from "./Pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
+import Tips from "./Pages/Tips";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path={"/signin"} element={<Login />} />
           <Route path={"/signup"} element={<Signup/>} />
           <Route path={"/"} element={<Home />} />
+          <Route path={"/tips"} element={<Tips/>}></Route>
         </Routes>
       </Router>
     </div>
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import Sidebar from '../Components/sidebar'
+// import Sidebar from '../Components/sidebar'
 import Footer from '../Components/Footer'
 import Testimonias from '../Components/Testimonials'
+import Tipscard from '../Components/Tipscard'
 
 export default function Home() {
   return (
     <div>
       
-        <Sidebar/>
+        {/* <Sidebar/> */}
         <Testimonias/>
 
         HOme Pages
         
         <Footer/>
+      
 
 
     </div>
