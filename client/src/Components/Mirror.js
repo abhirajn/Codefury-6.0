a41ef82cb901a7409f69a4749301d475c25abbb7
PremsagarChakrapani <premsagarc11@gmail.com>
2023-09-24 00:02:33 +0530

    testing?

---
+++ client/src/Components/Mirror.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Mirror.css'
+export default function Mirror() {
+  return (
+    <div>
+         <div class ="mirror" style=" background-image: url('https://images.pond5.com/african-woman-meditating-yoga-lotus-footage-077193422_prevstill.jpeg');">
+     <p> Looking for how to keep social media out of your lives?</p>
+    </div>
+    </div>
+  )
+}
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Sidebar from '../Components/Sidebar'
 import Footer from '../Components/Footer'
+import Mirror from '../Components/Mirror'
 import Testimonias from '../Components/Testimonials/Testimonials'
 import Login from './Login'
 import Signup from './Signup'
-import Header from './Header'
+import Header from '../Components/Testimonials/Header'
 import Tipscard from '../Components/Tipscard'
 import FAQ from '../Components/FAQ/FAQ'
 
@@ -13,6 +14,7 @@ export default function Home() {
     <div>
       <Header/>
         <Sidebar/>
+        <Mirror/>
      <FAQ/>
       <Testimonias/>
 
