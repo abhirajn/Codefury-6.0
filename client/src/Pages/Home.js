3d493a04f528b3baaf5385f136319318595fe2ba
Nithin63636 <naanunithi@gmail.com>
2023-09-23 15:27:58 +0530

    nithin on the way , move people

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import Sidebar from '../Components/sidebar'
+// import Sidebar from '../Components/sidebar'
 import Footer from '../Components/Footer'
 import Testimonias from '../Components/Testimonials'
+import Login from './Login'
+import Signup from './Signup'
 
 export default function Home() {
   return (
     <div>
       
-        <Sidebar/>
         <Testimonias/>
-
         HOme Pages
         
         <Footer/>
---
+++ try.py
@@ -0,0 +1,2 @@
+for i in range(51):
+    print('<span style={{"--i":{0}}}></span>'.format(i))
