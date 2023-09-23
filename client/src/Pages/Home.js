4bac0a42666eb6ebc4d215614092062b2c91a641
Nithin63636 <naanunithi@gmail.com>
2023-09-24 02:57:10 +0530

    pulling origin

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -5,10 +5,7 @@ import Mirror from '../Components/Mirror'
 import Testimonias from '../Components/Testimonials/Testimonials'
 import Login from './Login'
 import Signup from './Signup'
-<<<<<<< HEAD
-=======
 import Header from '../Components/Testimonials/Header'
->>>>>>> 5ba32c958530e4ea61f145607f8ace4d72fdb622
 import Tipscard from '../Components/Tipscard'
 import FAQ from '../Components/FAQ/FAQ'
 import Pricing from '../Components/Pricing'
